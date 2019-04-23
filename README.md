b01c601e5aa99e8f813a02f4004b894cbcf5173b
Arthur Dias <arthurdias.trad@icloud.com>
2019-04-22 22:51:19 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Toastmasters Timer
+
+A simple timer application to be used in timing Toastmasters speeches, whether they are prepared speeches, TableTopics or evalutations.
+
+By setting the minimum and maximum time, the app will signal green (minimum allowable time reached), yellow and red (maximum allowable time), providing for additional 30 seconds before the speaker is disqualified and cannot be voted as best speaker or evaluator.
+
+It is a single page application made using HTML and vanilla JS.
---
+++ timer.js
@@ -0,0 +1,60 @@
+var interval = null;
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector("#button").onclick = () => {
+    let min = document.getElementById("minimo").value;
+    let max = document.getElementById("maximo").value;
+    if (min == "" || max == "") {
+        alert("O tempo mínimo e máximo não podem estar vazios");
+    } else if (min > max) {
+        alert("O tempo máximo deve ser maior que o mínimo")
+    } else {
+    interval = setInterval(function() {count(min, max);}, 1000);
+    remover();
+    }
+    };
+});
+
+remover = () => {
+    document.querySelector("#tempos").remove();
+    let btn = document.createElement("button");
+    btn.innerHTML = "Parar"
+    btn.classList.add('btn');
+    btn.classList.add('btn-primary');
+    btn.id = 'parar';
+    document.querySelector("div").appendChild(btn);
+    document.querySelector("#parar").onclick = () => {
+    clearInterval(interval);
+    return false;
+    };
+};
+
+let s = 0;
+let m = 0;
+
+count = (min, max) => {
+    let time = `${m.toLocaleString(undefined, {minimumIntegerDigits: 2})}:${(s % 60).toLocaleString(undefined, {minimumIntegerDigits: 2})}`
+    min *= 60;
+    max *= 60;
+    let med = parseInt((min + max)/2)
+    s++;
+    if (s > 0 && (s % 60 === 0)) {
+    m++;
+    }
+    let bodyColor = document.querySelector('body');
+
+    document.querySelector('h1').innerHTML = time;
+    if (s >= min && s < med) {
+    bodyColor.style.backgroundColor = 'green';
+    } else if (s >= med && s < max) {
+    bodyColor.style.backgroundColor = 'yellow';
+    } else if (s >= max && s < max+30) {
+    bodyColor.style.backgroundColor = 'red';
+    } else if (s >= max+30) {
+    bodyColor.style.backgroundColor = 'purple';
+    document.querySelector('#start').innerHTML = "Desqualificar";
+    document.querySelector('#start').style.fontSize = "80px";
+    document.querySelector('#start').style.padding = "80px";
+    };
+
+    return false;
+};
